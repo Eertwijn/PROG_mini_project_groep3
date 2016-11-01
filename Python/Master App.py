2db51d1258204ee2e03d1a5e3d12c0b3dd50480b
Jeroen-Walter <jeroen.vankuilenburg@student.hu.nl>
2016-11-01 14:32:59 +0100

    Merge branch 'master' of https://github.com/Eertwijn/PROG_mini_project_groep3

--- Python/Master App.py
+++ Python/Master App.py
@@ -24,36 +24,41 @@ lijstVanStations = stationsLijst()
 
 
 def station_Kiezen(lijstVanStations):
-    invoer_station = input("Is dit uw huidige station? ")
+    #Er wordt gevraagt of utrecht het huidige station is
+    invoer_station = input("Is dit uw huidige station(Utrecht Centraal)? ")
     station = ""
-
+    #als Utrecht niet het huidige station is wordt er gevraagt waar de persoon zich bevindt
     if invoer_station == "Nee":
         while station not in lijstVanStations:
             station = input("Geef het station waar u zich bevindt: ")
     else:
         station = "Utrecht"
+    #Het ingevoerde station wordt geprint en teruggegeven.
     print("U heeft",station,"aangegeven.")
 
     return station
 
 
 def tijden_ophalen(station):
+    #inlogegevens.
     authdetails = ('sam.zandee@gmail.com', 'PR15gnkYhlxUuWrjXFnZ_yBHswBfR-clw1oYMkbMW7eeeNLD0sGd5A')
+    #de url die nodig is wordt bepaald door het station er aan te plakken
     apiurl = "http://webservices.ns.nl/ns-api-avt?station=" + station
-
+    #de reactie wordt opgevraagt
     response = requests.get(apiurl, auth=authdetails)
-
+    #de reactie wordt geparst
     vertrekXML = xmltodict.parse(response.text)
 
     nuttige_info = ""
-
+    #De vertrektijden worden opgezocht en op geslagen in nuttige_info=""
     for vertek in vertrekXML["ActueleVertrekTijden"]["VertrekkendeTrein"]:
         if "VertrekVertragingTekst" not in vertek:
             nuttige_info += "De trein naar {} vertrekt om {} op spoor {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekSpoor"]["#text"])
         else:
             nuttige_info += "De trein naar {} vertrekt om {} {} op spoor {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekVertragingTekst"], vertek["VertrekSpoor"]["#text"])
-
+    #de vertrektijden worden uitgeprint
     print (nuttige_info)
 
+#het programma wordt gedraait.
 station = station_Kiezen(lijstVanStations)
 tijden_ophalen(station)
