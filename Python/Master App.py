685e68df3848e5b45eeda051d1b33e664dcf8cd1
Ezri-Mudde <ezri.mudde@student.hu.nl>
2016-11-01 13:21:20 +0100

    optie voor taal toegevoegd

--- Python/Master App.py
+++ Python/Master App.py
@@ -1,7 +1,7 @@
 import requests
 import xmltodict
 
-def tijden_ophalen(station):
+def tijden_ophalen(station, taal):
     authdetails = ('sam.zandee@gmail.com', 'PR15gnkYhlxUuWrjXFnZ_yBHswBfR-clw1oYMkbMW7eeeNLD0sGd5A')
     apiurl = "http://webservices.ns.nl/ns-api-avt?station=" + station
 
@@ -11,10 +11,19 @@ def tijden_ophalen(station):
 
     nuttige_info = ""
 
-    for vertek in vertrekXML["ActueleVertrekTijden"]["VertrekkendeTrein"]:
-        if "VertrekVertragingTekst" not in vertek:
-            nuttige_info += "De trein naar {} vertrekt om {} op spoor {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekSpoor"]["#text"])
-        else:
-            nuttige_info += "De trein naar {} vertrekt om {} {} op spoor {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekVertragingTekst"], vertek["VertrekSpoor"]["#text"])
+    if taal == "NL":
+        for vertek in vertrekXML["ActueleVertrekTijden"]["VertrekkendeTrein"]:
+            if "VertrekVertragingTekst" not in vertek:
+                nuttige_info += "De trein naar {} vertrekt om {} op spoor {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekSpoor"]["#text"])
+            else:
+                nuttige_info += "De trein naar {} vertrekt om {} {} op spoor {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekVertragingTekst"], vertek["VertrekSpoor"]["#text"])
+    else:
+        for vertek in vertrekXML["ActueleVertrekTijden"]["VertrekkendeTrein"]:
+            if "VertrekVertragingTekst" not in vertek:
+                nuttige_info += "The train to {} leaves at {} on platform {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekSpoor"]["#text"])
+            else:
+                nuttige_info += "The train to {} leaves at {} {} on platform {}.\n".format(vertek["EindBestemming"], vertek["VertrekTijd"][11:16], vertek["VertrekVertragingTekst"], vertek["VertrekSpoor"]["#text"])
+    print(nuttige_info)
+    return nuttige_info
 
-    print (nuttige_info)
+#tijden_ophalen("ut", "NL") #om te proberen
