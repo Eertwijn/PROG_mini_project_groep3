dd318a4d34fe24168f45d0894d6bbdae8e146eea
Sam <Sam.zandee@gmail.com>
2016-11-02 19:51:32 +0100

    Nog even met die knoppen lopen klieren

--- Python/AnderStation.py
+++ Python/AnderStation.py
@@ -3,16 +3,18 @@ from tkinter import messagebox
 import MasterApp
 
 def taalNL():
-    koptekst["text"] = "Huidig Station"
+    koptekst["text"] = "Voer hier uw gewenste station in:"
     knopsluiten["text"] = "Venster Sluiten"
+    invulknop["text"] = "invullen"
     global taal
     taal = "NL"
     reisinformatie.delete(1.0, END)
     station_invullen()
 
 def taalENG():
-    koptekst["text"] = "Current Station"
+    koptekst["text"] = "Please enter your station here:"
     knopsluiten["text"] = "Close Window"
+    invulknop["text"] = "Typ"
     global taal
     taal = "ENG"
     reisinformatie.delete(1.0, END)
@@ -42,18 +44,34 @@ def venster_openen(meegeeftaal):
     global koptekst
     koptekst = Label(master=achterkant,
                   font = ('Raleway', 30),
-                  text='Huidig Station',
+                  text='Voer hier uw gewenste station in:',
                   background='#FFCD4C',
                   height=3
                   )
     koptekst.pack()
 
-    global entry
-    entry = Entry(master=achterkant)
-    entry.pack()
+    #
+    box1 = Frame(master=achterkant,
+                bg= '#FFCD4C'
+                )
+    box1.pack()
 
-    knop = Button(master=achterkant, text="Invullen", width=10, command=station_invullen)
-    knop.pack()
+    # Invoer balk
+    global entry
+    entry = Entry(master=box1,
+                  font= ('Raleway', 15),
+                  width= 15)
+    entry.pack(side= LEFT)
+
+    #Knop om de invoer actieveren
+    global invulknop
+    invulknop = Button(master=box1, text="Invullen",
+                  font = ('Raleway', 15),
+                  bg= '#053593',
+                  fg= 'white',
+                  width= 15,
+                  command=station_invullen)
+    invulknop.pack(side= RIGHT)
 
     global reisinformatie
     reisinformatie = Text(master=achterkant,
